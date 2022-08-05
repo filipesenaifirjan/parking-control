bdb27320122b9426ee84c899d590f7e39b0eb799
filipesenaifirjan <filipemattossenai@gmail.com>
2022-08-05 10:32:25 -0300

    criando packages

---
+++ parking/src/main/java/com/control/parking/models/ParkingSpotModel.java
@@ -0,0 +1,12 @@
+package com.control.parking.models;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+@Entity
+@Table(name = "TB_PARKING_SPOT")
+
+public class ParkingSpotModel implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
